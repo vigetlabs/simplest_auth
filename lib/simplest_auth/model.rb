69a7142dd8bb51835709546a19d4a3ddeeef5fa1
Tony Pitale <tpitale@aether.local>
2008-12-01 11:32:44 -0500

    started model module

--- lib/simplest_auth/model.rb
+++ lib/simplest_auth/model.rb
@@ -1,5 +1,30 @@
 module SimplestAuth
   module Model
+    def self.included(base)
+      base.send(:extend, ClassMethods)
+      base.send(:include, InstanceMethods)
+    end
     
+    module ClassMethods
+      def authenticate(email, password)
+        klass = find_by_email(email)
+        (klass && klass.authentic?(password)) ? klass : nil
+      end
+    end
+    
+    module InstanceMethods
+      # def authentic?(password)
+      #   Password.new(self.crypted_password) == password
+      # end
+
+      private  
+      # def hash_password
+      #   self.crypted_password = Password.create(self.password)
+      # end
+      # 
+      # def password_required?
+      #   self.crypted_password.blank? || !self.password.blank?
+      # end
+    end
   end
 end
--- test/unit/simplest_auth/model_test.rb
+++ test/unit/simplest_auth/model_test.rb
@@ -1,11 +1,21 @@
 require File.dirname(__FILE__) + '/../../test_helper'
 
-module SimplestAuth
-  class ModelTest < Test::Unit::TestCase
-    context "the Model module" do
-      should "have tests" do
-        assert true
-      end
+class User
+  include SimplestAuth::Model
+end
+
+class UserTest < Test::Unit::TestCase
+  context "the User class" do
+    should "find a user by email for authentication" do
+      user_stub = stub()
+      user_stub.stubs(:authentic?).with('password').returns(true)
+      User.stubs(:find_by_email).with('joe@schmoe.com').returns(user_stub)
+      
+      assert_equal user_stub, User.authenticate('joe@schmoe.com', 'password')
     end
+  end
+  
+  context "an instance of the User class" do
+    
   end
 end
