2ccde285748a148b94618c577a92f655e12abb93
Mike Ackerman <mikeackerman@gmail.com>
2013-10-16 14:56:56 -0600

    Adds gem's locale file to I18n load_path

--- lib/simplest_auth.rb
+++ lib/simplest_auth.rb
@@ -15,4 +15,7 @@ require 'simplest_auth/controller'
 require 'simplest_auth/sessions_controller'
 require 'simplest_auth/session'
 
-module SimplestAuth; end
+module SimplestAuth
+  spec = Gem::Specification.find_by_name("simplest_auth")
+  I18n.load_path += Dir.glob(File.join(spec.gem_dir, "config", "locales", "*.yml"))
+end
